c32719490c1014e1f5a84dbbb4829caf4c72d65c
jyotikacha123 <jyotikacha1234@gmail.com>
2026-01-15 10:41:44 +0530

    added processing

--- codes/Clean_data.py
+++ codes/Clean_data.py
@@ -1,2 +1,8 @@
 def clean():
   print("we are cleaning the data...")
+
+def load_data():
+  print("loading the Data....")
+
+  def processing ():
+    print("processing the files...")
--- codes/main.py
+++ codes/main.py
@@ -2,3 +2,4 @@ def add(val1,val2):
     return val1 + val2
 
 print(add(7,9))
+
